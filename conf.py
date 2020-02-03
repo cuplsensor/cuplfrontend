996acfd26f4dfa859544ea2db56cfa42ad195075
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-02-03 13:47:18 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -43,8 +43,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinxcontrib.openapi',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
