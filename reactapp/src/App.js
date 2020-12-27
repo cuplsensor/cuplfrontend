e208b2ed253033639774b20ea5e3b12b70694f16
Malcolm Mackay <malcolm@plotsensor.com>
2020-12-27 19:21:12 +0000

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -34,7 +34,7 @@ import AdminTagsAddPage from "./AdminTagsAddPage";
 
 function App() {
   return (
-   <Router>
+   <Router basename=process.env.REACT_APP_BASENAME>
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
