7d26c7a46fa4adc87a842842fbec4f4a44a782b1
Malcolm Mackay <malcolm@plotsensor.com>
2021-01-30 10:43:33 +0000

    Updated boilerplate with PyCharm.

--- reactapp/src/ConsumerVersionPage.jsx
+++ reactapp/src/ConsumerVersionPage.jsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021. Plotsensor Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from "react";
 import {withRouter } from "react-router-dom";
 import {ConsumerBasePage} from "./ConsumerPage";
@@ -76,7 +92,13 @@ class ConsumerVersionPage extends React.Component {
               <div className="content">
                   {versionlist}
               </div>
-
+              <div className="content">
+                  <p>Icons by
+                      <a href="https://fontawesome.com/"> FontAwesome</a>.
+                      Used under the
+                      <a href="https://creativecommons.org/licenses/by/4.0/deed.ast"> CC-BY 4.0 licence</a>.
+                  </p>
+              </div>
           </ConsumerBasePage>
       );
   }
